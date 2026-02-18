91ebedd6601da4d68e75b267c5fba1c75a50d006
Adhik Joshi <adhik3035@gmail.com>
2026-02-19 02:18:30 +0530

    feat: Export ModelsLab provider

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import Replicate from "./replicate.js";
 import Stability from "./stability.js";
 import A1111 from "./a1111.js";
+import ModelsLab from "./modelslab.js";
 
 export default async function Imagine(input, options = {}) {
     if (options.service == Imagine.REPLICATE) return await Replicate(input, options);
     if (options.service == Imagine.STABILITY) return await Stability(input, options);
+    if (options.service == Imagine.MODELSLAB) return await ModelsLab(input, options);
     return await A1111(input, options);
 }
 
 Imagine.REPLICATE = "replicate";
 Imagine.STABILITY = "stability";
 Imagine.A1111 = "a1111";
+Imagine.MODELSLAB = "modelslab";
